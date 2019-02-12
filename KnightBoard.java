f449ca7abf9d0111c2a13f421da20449e70b61e8
Devin Lin <Dlin01@stuy.edu>
2019-02-12 14:35:39 -0500

    finished tostring

--- KnightBoard.java
+++ KnightBoard.java
@@ -9,20 +9,41 @@ public class KnightBoard {
       }
     }
   }
-  public String toString()  {
 
+  public String toString() {
+    String ans = "";
+    if (board.length * board[0].length < 10) {
+      for (int row = 0; row < board.length; row++) {
+        for (int col = 0; col < board[row].length; col++) {
+          ans += board[row][col] + " ";
+        }
+        ans += "\n";
+      }
+    } else {
+      for (int row = 0; row < board.length; row++) {
+        for (int col = 0; col < board[row].length; col++) {
+          if (board[row][col] < 10) {
+            ans += " " + board[row][col] + " ";
+          } else {
+            ans += board[row][col] + " ";
+          }
+        }
+        ans += "\n";
+      }
+    }
+    return ans;
   }
   /**
-  @throws IllegalStateException when the board contains non-zero values.
-  @throws IllegalArgumentException when either parameter is negative
-   or out of bounds.
+   * @throws IllegalStateException when the board contains non-zero values.
+   * @throws IllegalArgumentException when either parameter is negative or out of bounds.
    */
   public boolean solve(int startingRow, int startingCol) {
 
   }
-  @throws IllegalStateException when the board contains non-zero values.
-  @throws IllegalArgumentException when either parameter is negative
-   or out of bounds.
+  /**
+   * @throws IllegalStateException when the board contains non-zero values.
+   * @throws IllegalArgumentException when either parameter is negative or out of bounds.
+  */
   public int countSolutions(int startingRow, int startingCol) {
 
   }
