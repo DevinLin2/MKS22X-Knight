07ba5f826136e9e820272ca8ae294ea47cc6f711
Devin Lin <Dlin01@stuy.edu>
2019-02-13 10:03:32 -0500

    added a valid move helper method

--- KnightBoard.java
+++ KnightBoard.java
@@ -34,6 +34,9 @@ public class KnightBoard {
     return ans;
   }
   /**
+   * Attempts to solve the Knights Tour given the starting row and col
+   * @param startingRow the starting row
+   * @param startingCol the starting col
    * @throws IllegalStateException when the board contains non-zero values.
    * @throws IllegalArgumentException when either parameter is negative or out of bounds.
    */
@@ -51,11 +54,26 @@ public class KnightBoard {
     if (startingRow > board.length-1 || startingCol > board[0].length-1){
       throw new IllegalArgumentException("Parameter(s) exceed array length");
     }
+    return solveHelper(startingRow, startingCol, 0);
+  }
+  private boolean solveHelper(int row, int col, int level) {
+    if (level == board.length * board[0].length) {
+      return true;
+    }
+    
+  }
+  private boolean validMove(int knightX, int knightY, int xMove, int yMove) {
+    if (knightX + xMove < board[0].length && knightX + xMove > 0 && KnightY + yMove < board.length && knightY + yMove > 0) {
+      return true;
+    }
   }
   /**
+   * Attempts to count all the solutions of the Knights Tour given the starting row and col
+   * @param startingRow the starting row
+   * @param startingCol the starting col
    * @throws IllegalStateException when the board contains non-zero values.
    * @throws IllegalArgumentException when either parameter is negative or out of bounds.
-  */
+   */
   public int countSolutions(int startingRow, int startingCol) {
     for (int row = 0; row < board.length; row++) {
       for (int col = 0; col < board[row].length; col++) {
