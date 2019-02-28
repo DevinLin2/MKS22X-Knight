c942244e0837dc34cab6d94e4972cde618c0efc9
Devin Lin <Dlin01@stuy.edu>
2019-02-27 21:59:55 -0500

    changed logic to adding moves to an array due to cases where a board is unsolvable; FINISHED

--- KnightBoard.java
+++ KnightBoard.java
@@ -11,13 +11,8 @@ public class KnightBoard {
     KnightBoard board = new KnightBoard(Integer.parseInt(args[0]), Integer.parseInt(args[1]));
     board.solve(0,0);
     System.out.println(board);
-    // runTest(0);
-    // runTest(1);
-    // runTest(2);
-    // runTest(3);
-    // runTest(4);
+    board.clear();
   }
-
   public KnightBoard(int startingRows,int startingCols) {
     board = new int[startingRows][startingCols];
     for (int row = 0; row < board.length; row++) {
@@ -63,30 +58,14 @@ public class KnightBoard {
         ans += "\n";
       }
     }
-
-    ans += "-----------------------------------------------------\n";
-    // this prints out the possible outgoing moves from each space
-    if (outgoingMoves.length * outgoingMoves[0].length < 10) {
-      for (int row = 0; row < outgoingMoves.length; row++) {
-        for (int col = 0; col < outgoingMoves[row].length; col++) {
-          ans += outgoingMoves[row][col] + " ";
-        }
-        ans += "\n";
-      }
-    } else {
-      for (int row = 0; row < outgoingMoves.length; row++) {
-        for (int col = 0; col < outgoingMoves[row].length; col++) {
-          if (outgoingMoves[row][col] < 10) {
-            ans += " " + outgoingMoves[row][col] + " ";
-          } else {
-            ans += outgoingMoves[row][col] + " ";
-          }
-        }
-        ans += "\n";
+    return ans;
+  }
+  private void clear() {
+    for (int row = 0; row < board.length; row++) {
+      for (int col = 0; col < board[row].length; col++) {
+        board[row][col] = 0;
       }
     }
-
-    return ans;
   }
   /**
    * Attempts to solve the Knights Tour given the starting row and col
@@ -137,7 +116,51 @@ public class KnightBoard {
     }
     return false;
   }
+  private void debug() {
+    String ans = "";
+    if (board.length * board[0].length < 10) {
+      for (int r = 0; r < board.length; r++) {
+        for (int c = 0; c < board[r].length; c++) {
+          ans += board[r][c] + " ";
+        }
+        ans += "\n";
+      }
+    } else {
+      for (int r = 0; r < board.length; r++) {
+        for (int c = 0; c < board[r].length; c++) {
+          if (board[r][c] < 10) {
+            ans += " " + board[r][c] + " ";
+          } else {
+            ans += board[r][c] + " ";
+          }
+        }
+        ans += "\n";
+      }
+    }
+    ans += "--------------------------------\n";
+    if (outgoingMoves.length * outgoingMoves[0].length < 10) {
+      for (int r = 0; r < board.length; r++) {
+        for (int c = 0; c < board[r].length; c++) {
+          ans += outgoingMoves[r][c] + " ";
+        }
+        ans += "\n";
+      }
+    } else {
+      for (int r = 0; r < board.length; r++) {
+        for (int c = 0; c < board[r].length; c++) {
+          if (outgoingMoves[r][c] < 10) {
+            ans += " " + outgoingMoves[r][c] + " ";
+          } else {
+            ans += outgoingMoves[r][c] + " ";
+          }
+        }
+        ans += "\n";
+      }
+    }
+    System.out.println(ans);
+  }
   private boolean solveHelper(int row, int col, int level) {
+    debug();
     int[] moves = new int[] {2, 1, 2, -1, -2, 1, -2, -1, 1, 2, 1, -2, -1, 2, -1, -2};
     if (level == board.length * board[0].length + 1) {
       return true;
@@ -153,58 +176,59 @@ public class KnightBoard {
   }
 
   // optimized solve using Warnsdorff's algorithm
+  private boolean contains(int y, int x, ArrayList<Integer> moves) {
+    if (moves.size() == 0) {
+      return false;
+    }
+    for (int i = 0; i < moves.size(); i += 2) {
+      if (y == moves.get(i) && x == moves.get(i+1)) {
+        return true;
+      }
+    }
+    return false;
+  }
   private boolean optimizedSolveHelper(int row, int col, int level) {
     int[] moves = new int[] {2, 1, 2, -1, -2, 1, -2, -1, 1, 2, 1, -2, -1, 2, -1, -2};
     ArrayList<Integer> outgoingM = new ArrayList<Integer>();
+    ArrayList<Integer> outgoingMoveCoords = new ArrayList<Integer>();
     int lowest = 0;
     int index = 0;
-    int x = 0;
-    int y = 0;
-
+    //debug();
     if (level == board.length * board[0].length + 1) {
       return true;
     }
-    for (int i = 0; i < moves.length; i += 2) {
-      if (addKnight(row, col, level)) {
+    for (int moveIndex = 0; moveIndex < moves.length; moveIndex += 2) {
+      if (row + moves[moveIndex] < board.length && row + moves[moveIndex] >= 0 && col + moves[moveIndex + 1] < board[0].length && col + moves[moveIndex + 1] >= 0 &&
+      outgoingMoves[row + moves[moveIndex]][col + moves[moveIndex + 1]] != 0 && board[row + moves[moveIndex]][col + moves[moveIndex + 1]] == 0) {
+        outgoingM.add(outgoingMoves[row + moves[moveIndex]][col + moves[moveIndex + 1]]); // adds all the possible outgoing moves from each possible move to an ArrayList
+      }
+    }
+    if (outgoingM.size() > 0) {
+      Collections.sort(outgoingM);
+      // makes an ArrayList of the coords prioritizing moves with lower outgoing moves
+      for (int i = 0; i < outgoingM.size(); i++) {
         for (int moveIndex = 0; moveIndex < moves.length; moveIndex += 2) {
           if (row + moves[moveIndex] < board.length && row + moves[moveIndex] >= 0 && col + moves[moveIndex + 1] < board[0].length && col + moves[moveIndex + 1] >= 0 &&
-          outgoingMoves[row + moves[moveIndex]][col + moves[moveIndex + 1]] != 0 && board[row + moves[moveIndex]][col + moves[moveIndex + 1]] == 0) {
-            outgoingM.add(outgoingMoves[row + moves[moveIndex]][col + moves[moveIndex + 1]]); // adds all the possible outgoing moves from each possible move to an ArrayList
+          outgoingMoves[row + moves[moveIndex]][col + moves[moveIndex + 1]] == outgoingM.get(i) && !(contains(moves[moveIndex], moves[moveIndex + 1], outgoingMoveCoords))) {
+            outgoingMoveCoords.add(moves[moveIndex]);
+            outgoingMoveCoords.add(moves[moveIndex+1]);
           }
         }
-        if (outgoingM.size() > 0) {
-          Collections.sort(outgoingM);
-          while (lowest == 0) { // gets the lowest value in ArrayList of possible outgoing moves
-            lowest = outgoingM.get(index);
-            index++;
-          }
-          index--;
-          //System.out.println("lowest1: " + lowest);
-          index = 0; // resets index
-          // now we have the lowest possible outgoing moves
-          for (int moveIndex = 0; moveIndex < moves.length; moveIndex += 2) {
-            if (row + moves[moveIndex] < board.length && row + moves[moveIndex] >= 0 && col + moves[moveIndex + 1] < board[0].length && col + moves[moveIndex + 1] >= 0 &&
-            // checks for the x and y value of the place with the lowest outgoing moves and checks that it hasent been stepped on yet
-            outgoingMoves[row + moves[moveIndex]][col + moves[moveIndex + 1]] == lowest && board[row + moves[moveIndex]][col + moves[moveIndex + 1]] == 0) {
-              y = row + moves[moveIndex];
-              x = col + moves[moveIndex + 1];
-            }
-          }
-          outgoingM.clear();
-        } else {
-          for (int z = 0; z < moves.length; z += 2) {
-            if (addKnight(row, col, level) && solveHelper(row + moves[z], col + moves[z+1], level + 1)) {
-              return true;
-            } else {
-              removeKnight(row, col, level);
-            }
-          }
-        }
-        if (optimizedSolveHelper(y, x, level + 1)) {
+      }
+      for (int z = 0; z < outgoingMoveCoords.size(); z += 2) {
+        if (addKnight(row, col, level) && optimizedSolveHelper(row + outgoingMoveCoords.get(z), col + outgoingMoveCoords.get(z+1), level + 1)) {
           return true;
+        } else {
+          removeKnight(row, col, level);
         }
-      } else {
-        removeKnight(row, col, level);
+      }
+    } else {
+      for (int z = 0; z < moves.length; z += 2) {
+        if (addKnight(row, col, level) && optimizedSolveHelper(row + moves[z], col + moves[z+1], level + 1)) {
+          return true;
+        } else {
+          removeKnight(row, col, level);
+        }
       }
     }
     return false;
@@ -248,27 +272,5 @@ public class KnightBoard {
     }
     removeKnight(r, c, level);
     return ans;
-  }
-  public static void runTest(int i){
-    KnightBoard b;
-    int[]m =   {4,5,5,5,5};
-    int[]n =   {4,5,4,5,5};
-    int[]startx = {0,0,0,1,2};
-    int[]starty = {0,0,0,1,2};
-    int[]answers = {0,304,32,56,64};
-    if(i >= 0 ){
-      try{
-        int correct = answers[i];
-        b = new KnightBoard(m[i%m.length],n[i%m.length]);
-        int ans  = b.countSolutions(startx[i],starty[i]);
-        if(correct==ans){
-          System.out.println("PASS board size: "+m[i%m.length]+"x"+n[i%m.length]+" "+ans);
-        }else{
-          System.out.println("FAIL board size: "+m[i%m.length]+"x"+n[i%m.length]+" "+ans+" vs "+correct);
-        }
-      }catch(Exception e){
-        System.out.println("FAIL Exception case: "+i);
-      }
-    }
   }
 }
